4d8c2620a621517f94c4e8dbb394cb07fe12558c
yllnor13 <yllnor13@gmail.com>
2021-02-11 00:47:40 +0100

    Sender filer over til github. Foerste commit

---
+++ app/src/main/java/com/example/yllnor_oblig2/AlpacaParty.kt
@@ -0,0 +1,9 @@
+package com.example.yllnor_oblig2
+
+class AlpacaParty (thisid : String, thisname : String, thisleader : String, thisimg : String, thiscolor : String){
+    val id : String = thisid
+    val name : String = thisname
+    val leader : String = thisleader
+    val img : String = thisimg
+    val color : String = thiscolor
+}
